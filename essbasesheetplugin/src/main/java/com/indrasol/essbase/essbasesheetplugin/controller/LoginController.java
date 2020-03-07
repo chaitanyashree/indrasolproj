0d36d27b3b6a9c4e04b6a7e5b05fdf66c3650633
chaitanyashree <chaitanyshree786@gmail.com>
2020-03-07 15:53:28 +0530

    connection changes doe

--- essbasesheetplugin/src/main/java/com/indrasol/essbase/essbasesheetplugin/controller/LoginController.java
+++ essbasesheetplugin/src/main/java/com/indrasol/essbase/essbasesheetplugin/controller/LoginController.java
@@ -1,7 +1,9 @@
 package com.indrasol.essbase.essbasesheetplugin.controller;
 
 import com.indrasol.essbase.essbasesheetplugin.model.Credentials;
+import com.indrasol.essbase.essbasesheetplugin.service.LoginService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/essbase")
 public class LoginController {
 
+    private LoginService loginService;
+
+    @Autowired
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
 
 
     @GetMapping("/login")
@@ -20,9 +28,25 @@ public class LoginController {
         String s_provider = "http://ec2-13-127-165-106.ap-south-1.compute.amazonaws.com:28080/aps/JAPI"; 
 
 
-        Credentials cred = new Credentials(s_userName, s_password, s_olapSvrName, s_provider);
-        return cred;
+        Credentials credentials = new Credentials(s_userName, s_password, s_olapSvrName, s_provider);
+        loginService.login(credentials);
+        System.out.println("login sucessfully...");
+        //loginService.loadData();
+        
+        credentials =  loginService.getEssbaseConnection().getCredentials();
 
+        return credentials;
     } 
+
+    @GetMapping("/logout")
+    public String logout() {
+        loginService.disconnect();
+        return "Logout";
+    }
+
+    @GetMapping("/loaddata") 
+    public String loadData(){
+        return loginService.loadData();
+    }
 
 }
