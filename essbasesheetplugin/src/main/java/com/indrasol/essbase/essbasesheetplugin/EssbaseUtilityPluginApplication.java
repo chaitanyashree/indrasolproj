d98a86b6c702d81896828126d8add7b39467b853
chaitanyashree <chaitanyashree786@gmail.com>
2020-03-15 22:52:42 +0530

    jlib working fine

--- essbasesheetplugin/src/main/java/com/indrasol/essbase/essbasesheetplugin/EssbaseUtilityPluginApplication.java
+++ essbasesheetplugin/src/main/java/com/indrasol/essbase/essbasesheetplugin/EssbaseUtilityPluginApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EssbaseUtilityPlugiApplication {
+public class EssbaseUtilityPluginApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EssbaseUtilityPlugiApplication.class, args);
+		SpringApplication.run(EssbaseUtilityPluginApplication.class, args);
 	}
 
 }
